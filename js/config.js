44853cff91d0607842b003037377e87140f163a9
codencoding <36287664+Loegrath@users.noreply.github.com>
2019-11-03 02:35:35 -0800

    First draft of site structure and design.

---
+++ js/config.js
@@ -0,0 +1,72 @@
+var today = new Date();
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var date;
+var clockDate;
+var clockTime;
+
+
+
+window.onload = function() {
+    clockDate = document.getElementById("clockDate");
+    clockTime = document.getElementById("clockTime");
+    updateDate();
+    runClock()
+    var myVar = setInterval(runClock, 1000);
+}
+
+
+
+function runClock() {
+    let today = new Date();
+
+    let seconds = today.getSeconds();
+    if(parseInt(seconds) < 10) {
+        seconds = '0' + seconds;
+    }
+
+    let minutes = today.getMinutes()
+    if(parseInt(minutes) < 10) {
+        minutes = '0' + minutes;
+    }
+
+    let hours = today.getHours()
+    let meridiem = "AM";
+    if(hours >=12) {
+        meridiem = "PM";
+    }
+    hours %= 12;
+    if(hours == 0) {
+        hours = "12";
+    }
+
+    if(meridiem == "AM" && hours == 12) {
+        updateDate();
+    }
+
+    let time = hours + ":" + minutes + ' ' + meridiem;
+    // In case I ever want to have live seconds again.
+    // let time = hours + ":" + minutes + ":" + seconds + ' ' + meridiem;
+
+    
+    clockTime.innerHTML = time;
+}
+
+function updateDate() {
+    today = new Date();
+    let day = today.getDate();
+    let dayModifier;
+    if(day % 10 == 1){
+        dayModifier = "st"
+    } else if (day % 10 == 2) {
+        dayModifier = "nd"
+    } else if (day % 10 == 3) {
+        dayModifier = "rd"
+    } else {
+        dayModifier = "th"
+    }
+    date = monthNames[today.getMonth()] + ' ' + day + dayModifier + ' ' + today.getFullYear();
+
+    clockDate.innerHTML = date;
+}
