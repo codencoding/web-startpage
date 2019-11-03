773240e8a438f6d0da45ab8cc05cafd5b249eefd
Chase O <36287664+codencoding@users.noreply.github.com>
2019-11-03 15:36:23 -0800

    Images (#1)
    * Working CSS to correctly put images into table.
    
    * Icon linking completed.

--- js/config.js
+++ js/config.js
@@ -2,20 +2,22 @@ var today = new Date();
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 var date;
 var clockDate;
 var clockTime;
-
-
+var iconElems;
 
 window.onload = function() {
     clockDate = document.getElementById("clockDate");
     clockTime = document.getElementById("clockTime");
+    iconElems = document.getElementsByClassName("iconRow");
+    // console.log(iconElems[0].cells);
     updateDate();
-    runClock()
+    runClock();
     var myVar = setInterval(runClock, 1000);
+    updateLogoLinks();
 }
-
 
 
 function runClock() {
@@ -26,12 +28,12 @@ function runClock() {
         seconds = '0' + seconds;
     }
 
-    let minutes = today.getMinutes()
+    let minutes = today.getMinutes();
     if(parseInt(minutes) < 10) {
         minutes = '0' + minutes;
     }
 
-    let hours = today.getHours()
+    let hours = today.getHours();
     let meridiem = "AM";
     if(hours >=12) {
         meridiem = "PM";
@@ -48,7 +50,6 @@ function runClock() {
     let time = hours + ":" + minutes + ' ' + meridiem;
     // In case I ever want to have live seconds again.
     // let time = hours + ":" + minutes + ":" + seconds + ' ' + meridiem;
-
     
     clockTime.innerHTML = time;
 }
@@ -58,15 +59,48 @@ function updateDate() {
     let day = today.getDate();
     let dayModifier;
     if(day % 10 == 1){
-        dayModifier = "st"
+        dayModifier = "st";
     } else if (day % 10 == 2) {
-        dayModifier = "nd"
+        dayModifier = "nd";
     } else if (day % 10 == 3) {
-        dayModifier = "rd"
+        dayModifier = "rd";
     } else {
-        dayModifier = "th"
+        dayModifier = "th";
     }
-    date = monthNames[today.getMonth()] + ' ' + day + dayModifier + ' ' + today.getFullYear();
+    date = dayNames[today.getDay()] + ", " + monthNames[today.getMonth()] + ' ' + day + dayModifier + ' ' + today.getFullYear();
 
     clockDate.innerHTML = date;
+}
+
+function updateLogoLinks() {
+    let linkNode;
+    let imgNode;
+    let cellValue;
+    let siteName;
+    let siteDomain;
+
+    for(row of iconElems) {
+        for(elem of row.cells) {
+            cellValue = elem.innerHTML;
+            if(cellValue == '') {
+                cellValue = "placeHolder.com";
+                elem.className = "inactiveIcon";
+            } else {
+                elem.className = "activeIcon";
+            }
+            siteName = cellValue.split('.')[0];
+            siteDomain = cellValue.split('.').slice(1,);
+
+            linkNode = document.createElement("a");
+            imgNode = document.createElement("img");
+            imgNode.className = "grow";
+
+            linkNode.href="https://" + siteName + "." + siteDomain.join('.');
+            imgNode.src="include/icons/" + siteName + ".png"
+
+            elem.innerHTML = '';
+            elem.appendChild(linkNode);
+            linkNode.appendChild(imgNode);
+        }
+    }
 }
