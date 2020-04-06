34265e2268e02a0b082c36ef0b81b4c3be6ac5a2
codencoding <36287664+Loegrath@users.noreply.github.com>
2020-04-06 14:04:55 -0700

    Removed search bar to prevent google search page redirect bug.

--- js/runtime.js
+++ js/runtime.js
@@ -2,9 +2,6 @@ var clockDate = document.getElementById("clockDate");
 var clockTime = document.getElementById("clockTime");
 var iconElems = document.getElementsByClassName("iconRow");
 var launch_icons_div = document.getElementById("launchIcons");
-var searchBar = document.getElementById("searchBar");
-searchBar.value = '';
-
 
 updateDate();
 runClock();
@@ -15,13 +12,3 @@ create_icons_table(launch_icons_div, icon_cfg["ROWS"], icon_cfg["COLUMNS"])
 if(isOnline){
     loadHeavyElems();
 }
-
-searchBar.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      // Trigger the button element with a click
-      googleSearch(searchBar.value);
-    }
-  }); 
