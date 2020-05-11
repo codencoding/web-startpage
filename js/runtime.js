860644964607d09ef01d03d1cddc0925f8747d4d
codencoding <36287664+Loegrath@users.noreply.github.com>
2020-05-11 12:59:18 -0700

    Adding back in search bar

--- js/runtime.js
+++ js/runtime.js
@@ -2,6 +2,8 @@ var clockDate = document.getElementById("clockDate");
 var clockTime = document.getElementById("clockTime");
 var iconElems = document.getElementsByClassName("iconRow");
 var launch_icons_div = document.getElementById("launchIcons");
+var searchBar = document.getElementById("searchBar");
+searchBar.value = '';
 
 updateDate();
 runClock();
@@ -12,3 +14,13 @@ create_icons_table(launch_icons_div, icon_cfg["ROWS"], icon_cfg["COLUMNS"])
 if(isOnline){
     loadHeavyElems();
 }
+
+searchBar.addEventListener("keyup", function(event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      googleSearch(searchBar.value);
+    }
+  }); 
